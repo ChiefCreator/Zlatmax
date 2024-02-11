2b0f053dd50df907ab925a6a488e010aa9f8bcb6
Kirill Pekarskiy <kipe122006@gmail.com>
2024-02-11 15:55:12 +0300

    Создание подвала сайта

--- src/js/index.js
+++ src/js/index.js
@@ -171,3 +171,35 @@ const swiperNovelty = new Swiper('.new__slider', {
         },
     }
 });
+
+let spoilers = document.querySelectorAll(".spoiler");
+spoilers.forEach(spoiler => {
+    let headerSpoiler = spoiler.querySelector(".spoiler-header");
+    let menuSpoiler = spoiler.querySelector(".spoiler-body");
+    let listSpoiler = spoiler.querySelector(".spoiler-content");
+    let arrow = spoiler.querySelector(".spoiler-header img");
+
+    headerSpoiler.addEventListener("click", function() {
+        let actMenuSpoiler = document.querySelector(".spoiler-body_open");
+        let actListSpoiler = document.querySelector(".spoiler-content_open");
+        let actArrow = document.querySelector(".spoiler-arrow-act");
+
+        if (actMenuSpoiler && actMenuSpoiler != menuSpoiler) {
+            actMenuSpoiler.classList.remove("spoiler-body_open");
+            actListSpoiler.classList.remove("spoiler-content_open");
+            actArrow.classList.remove("spoiler-arrow-act");
+        }
+
+        arrow.classList.toggle("spoiler-arrow-act");
+        menuSpoiler.classList.toggle("spoiler-body_open");
+        listSpoiler.classList.toggle("spoiler-content_open");
+
+    })
+})
+
+let checks = document.querySelectorAll(".newsletter-form__check-wrapper");
+checks.forEach(check => {
+    check.addEventListener("click", function() {
+        check.classList.toggle("newsletter-form__check-wrapper_act");
+    })
+})
